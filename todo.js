b346ad5d08e59b39ca9b12ce7b767f186aa74124
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-09 14:07:26 -0600

    Inherit Prototypes

--- todo.js
+++ todo.js
@@ -23,20 +23,33 @@ Todo.prototype.isDone = function() {
   return 'Nope';
 };
 
-var WorkTodo = function(title) {
+var WorkTodo = function(title, floor) {
   // Run the Todo constructor with the current context `this`
   Todo.call(this, title, 100);
+  this.floor = floor;
 
   // Same just using `apply`
   // Todo.apply(this, [title, 100]);
+};
+
+WorkTodo.prototype = Object.create(Todo.prototype);
+
+WorkTodo.prototype.isDone = function() {
+  if (this.done) {
+    return 'Yup';
+  }
+
+  return `Yeah... I'm gonna need you to come in on Saturday...`;
 };
 
 // Create instances of Todo
 var x = new Todo('Cereal');
 var y = new Todo('Milk', 10);
 var z = new Todo('Toothpaste', 6);
-var a = new WorkTodo('Get Stapler');
+var a = new WorkTodo('Get Stapler', 20);
 console.log(a.title);
+console.log(a.importance);
+console.log(a.isDone());
 
 console.log(y.title);
 
