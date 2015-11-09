f4caf5d439afc6a7735c3ce0be198bfc29038a9b
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-09 11:34:35 -0600

    Prototype

--- todo.js
+++ todo.js
@@ -3,11 +3,21 @@ var Todo = function(title, importance) {
   this.done = false;
   this.importance = importance || 1;
   this.dateComplete = null;
+};
 
-  this.markComplete = function() {
-    this.done = true;
-    this.dateComplete = new Date();
-  };
+Todo.prototype.thisExists = 'Hello World';
+
+Todo.prototype.markComplete = function() {
+  this.done = true;
+  this.dateComplete = new Date();
+};
+
+Todo.prototype.isDone = function() {
+  if (this.done) {
+    return 'Yup';
+  }
+
+  return 'Nope';
 };
 
 var x = new Todo('Cereal');
@@ -15,7 +25,13 @@ var y = new Todo('Milk', 10);
 var z = new Todo('Toothpaste', 6);
 
 console.log(y.title);
+y.thisExists = 'Local';
+console.log(y.thisExists);
+console.log(z.thisExists);
+
+// Is markComplete share across all Todo instances
+console.log(z.markComplete === y.markComplete);
+
 x.markComplete();
-console.log(z.title);
-console.log(z.done);
-console.log(x.done);
+console.log(x.isDone());
+console.log(y.isDone());
